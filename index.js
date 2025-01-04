a2a017f9209397a39c3b3c646f6935555651e9c3
arojitmondal996e <arojitmondal996@gmail.com>
2025-01-04 19:51:22 +0600

    complete

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors({ origin: 'https://arojit-mondal.netlify.app' }));
-app.use(cors());
+app.use(cors({ origin: 'https://arojit-mondal.netlify.app' }));
+// app.use(cors());
 app.use(bodyParser.json());
 
 // Route to send email
